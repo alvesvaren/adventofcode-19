e3863f05ac08f27b572d366615abf8c860e770dd
Alve Svarén <alve@hotmail.se>
2019-12-02 18:36:54 +0100

    Moved actions into ifmain

--- get_inputs.py
+++ get_inputs.py
@@ -10,19 +10,20 @@ with open("config.json") as file:
 def get_day(number: int) -> str:
     return requests.get(f"https://adventofcode.com/2019/day/{number}/input", cookies=config["getInput"]["cookies"]).text
 
+if __name__ == "__main__":
+    input_path: str = os.path.join(os.getcwd(), "inputs")
+    current_day: int = datetime.now().day
 
-input_path: str = os.path.join(os.getcwd(), "inputs")
-current_day: int = datetime.now().day
+    if not os.path.exists(input_path):
+        os.mkdir(input_path)
 
-if not os.path.exists(input_path):
-    os.mkdir(input_path)
-try:
-    last_day_downloaded = tuple(
-        map(lambda x: int(x.rstrip(".txt")), tuple(os.walk(input_path))[0][-1]))[-1]
-except IndexError:
-    last_day_downloaded = 1
+    try:
+        last_day_downloaded = tuple(
+            map(lambda x: int(x.rstrip(".txt")), tuple(os.walk(input_path))[0][-1]))[-1]
+    except IndexError:
+        last_day_downloaded = 1
 
-if last_day_downloaded < current_day:
-    for i in range(last_day_downloaded, current_day+1):
-        with open(f"./inputs/{i}.txt", "w") as file:
-            file.write(get_day(i))
+    if last_day_downloaded < current_day:
+        for i in range(last_day_downloaded, current_day+1):
+            with open(f"./inputs/{i}.txt", "w") as file:
+                file.write(get_day(i))
